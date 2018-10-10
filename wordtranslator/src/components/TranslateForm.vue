79c356c63f8a34fd80cba1af48cb5bee1b16dfb5
Deonta Kilpatrick <deonta.kilpatrick@gmail.com>
2018-10-09 20:37:13 -0500

    Setting up initial translatorForm components

---
+++ wordtranslator/src/components/TranslateForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="translateForm">
+    <form v-on:submit="formSubmit">
+      <input type="text" v-model="textToTranslate" placeholder="Enter a word...">
+      <input type="submit" value="Translate">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'translateForm',
+  data () {
+    return {
+      textToTranslate: ''
+    }
+  },
+  methods: {
+    formSubmit (e) {
+      alert('Submitted')
+      e.preventDefault()
+    }
+  }
+}
+</script>
+
+<style>
+</style>
