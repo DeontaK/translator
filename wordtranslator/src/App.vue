336fd93af58ddd1dc21a760d8f6f6f0f99e0b9cd
Deonta Kilpatrick <deonta.kilpatrick@gmail.com>
2018-10-09 21:27:39 -0500

    Initial Setup for Translate Output component

--- wordtranslator/src/App.vue
+++ wordtranslator/src/App.vue
@@ -3,23 +3,31 @@
     <h1>Word Translator</h1>
     <h5>Powered by Vue.js</h5>
     <translate-form v-on:formSubmit="translateText"></translate-form>
+    <translate-output v-text="translatedText"></translate-output>
   </div>
 </template>
 
 <script>
 import TranslateForm from './components/TranslateForm'
+import TranslateOutput from './components/TranslateOutput'
 
 export default {
   name: 'App',
   components: {
-    TranslateForm
+    TranslateForm,
+    TranslateOutput
+  },
+  data: function () {
+    return {
+      translatedText: ''
+    }
   },
   methods: {
     translateText: function (text) {
       this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20181009T162704Z.80edfd10041c9342.892f1476cdef95a09af19e0ca58ab194a3bedf33&lang=ru&text=' + text)
-      .then((response) => {
-        console.log(response)
-      })
+        .then((response) => {
+          this.translatedText = response.body.text[0]
+        })
     }
   }
 }
